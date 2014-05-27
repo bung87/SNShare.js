03e4d08efbddbc13c0b8f260a10a00880a17fea8
bung87 <zh.bung@gmail.com>
2014-05-27 13:50:06 +0800

    remove grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,8 +134,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('build',['prepare:dist','replace:dist','fancySprites:dist','sass:dist','uglify:build','copy:main']);
-  grunt.registerTask('default', ['prepare:dist','replace:dist','fancySprites:dist','sass:dist','uglify:build']);
-    grunt.registerTask('test','test',function(){
+  // grunt.registerTask('default', ['prepare:dist','replace:dist','fancySprites:dist','sass:dist','uglify:build']);
+    grunt.registerTask('run','test',function(){
       if(grunt.file.isDir('dist')){
         
       }else{
